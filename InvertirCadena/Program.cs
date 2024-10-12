f73f6917c0365954b56f87c4b89268b43107ef06
XxZ0UGR4xX <gaelsolis653@gmail.com>
2024-10-11 22:36:50 -0600

    subir

---
+++ InvertirCadena/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace InvertirCadena
+{
+    internal class Program
+    {
+
+        class Programa
+        {
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Programa para invertir una cadena");
+                Console.WriteLine("Ingresa una cadena de texto");
+                String cadena = Console.ReadLine();
+
+                string cadenaInvertida = InvertirCadena(cadena);
+
+                Console.WriteLine("Cadena Invertida {0}", cadenaInvertida);
+            }
+
+            private static string InvertirCadena(string cadena)
+            {
+                char[] caracteres = cadena.ToCharArray();
+                string cadenaConvertida = "";
+                for (int i = cadena.Length - 1; i >= 0; i--)
+                {
+                    cadenaConvertida += caracteres[i];
+                }
+                return cadenaConvertida;
+            }
+
+        }
+
+    }
+}
